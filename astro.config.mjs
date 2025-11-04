7f998a791e7efe068c36c9b785356f94b2696d39
Pascal Hellmann <pascal@Mac.fritz.box>
2025-11-04 15:31:01 +0100

    Enable Cloudflare adapter for functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://faktum-app.de',
-    outDir: './dist',
+  site: 'https://faktum-app.de',
+
+  // Wichtig: Serverseitiger Output aktivieren
+  output: 'server',
+
+  // Cloudflare Pages Adapter aktivieren
+  adapter: cloudflare(),
+
+  // Optional, kannst du behalten
+  outDir: './dist',
 });
+
