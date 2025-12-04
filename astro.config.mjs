ae4a60f21492eddc9eb5f4de8c67972808b977f7
Pascal Hellmann <pascal@Mac.fritz.box>
2025-12-04 12:19:21 +0100

    Update: package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://faktum-app.de',
   output: 'server',
   adapter: cloudflare(),
   image: {
     service: {
-      entrypoint: 'astro/assets/services/noop', // Kein sharp
+      entrypoint: 'astro/assets/services/sharp',
+      config: {},
     },
   },
 });
